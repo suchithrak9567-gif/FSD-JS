26e2d86aa8eefbfbe1a0c6554cd26a11574238c0
suchithrak9567-gif <suchithrak9567@gmail.com>
2026-07-22 00:03:15 +0530

    Add session-13.js

---
+++ session-13.js
@@ -0,0 +1,233 @@
+//console.log("Hello, World!");
+// let name="John";
+// // console.log("name =",name);
+// console.log(typeof name);
+
+// let age="30";
+// console.log(age);
+// console.log(typeof age);
+
+// let age=30;
+// console.log(age);
+// console.log(typeof age);
+
+// let age =Number("30");
+// console.log(age);
+// console.log(typeof age);    
+
+// console.log(Number("abc")); // NaN
+// console.log(Number("")); // 0
+// console.log(boolean("true")); // 1
+// console.log(boolean("false")); // 0
+
+// console.log(typeof String(true));
+
+// console.log(typeof String(22));
+
+// let age=number("30");
+// let city; 
+// console.log(typeof undefined); // undefined
+// console.log(String(undefined)); // "undefined"
+// console.log(typeof null); // object
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean("true"));
+// console.log(Boolean("false"));
+// console.log(typeof Boolean("true"));
+// console.log(Boolean("1"));
+// console.log(Boolean("90"));
+// console.log(Boolean("")); // false
+// console.log(Boolean(null)); // false
+// console.log(Boolean(undefined)); // false
+// console.log(Boolean("0")); // true
+// console.log(Boolean(" ")); // true
+// console.log(Boolean(NaN)); // false
+
+// console.log(10+2)
+// console.log(10+2+"2");
+// console.log("10"+2);
+// console.log(10+"2");
+// console.log("10"+2+3);
+// let cal="10"-2
+// console.log(typeof cal);
+// console.log("5"*2);
+// console.log("5"/2);
+// console.log("5"%"2");
+
+// Arithmetic operators
+
+
+// console.log(10+10);
+// console.log(10-5);
+// console.log(10*5);
+// console.log(10/5);
+// console.log(10%5);
+// console.log(10**2); // 10^2
+// console.log(10**3); // 10^3
+// console.log(10**4); // 10^4
+
+// Comparison operators
+// console.log(10>5);
+// console.log(10<5);
+// console.log(10>=5);
+// console.log(10<=5);
+// console.log(10==5);
+// console.log(10!=5);
+// console.log(10==="10");
+// console.log(10=="10");
+// console.log(10!=="10");
+// console.log(10!="10");
+// console.log(10==="10");
+// console.log(10=="10");
+
+// Assignment operators
+// let x = 10;
+// console.log(x);
+// x += 5; // x = x + 5
+// console.log(x);
+// x -= 3; // x = x - 3
+// console.log(x);
+// x *= 2; // x = x * 2
+// console.log(x);
+// x /= 4; // x = x / 4
+// console.log(x);
+// x %= 3; // x = x % 3
+// console.log(x);
+// x **= 2; // x = x ** 2
+// console.log(x);
+
+// let score=10
+// score+=5;
+// console.log(score);
+
+
+// // logical operators
+// console.log(true && true); // true
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+// console.log(!true);
+// console.log(!false);
+
+
+// condition statement
+// if ,if else, else if, switch case
+// let score=90;
+// if(score>=90){
+//     console.log("A grade");
+// }
+// let score=80;
+// if(score<=90){
+//     console.log("A grade");
+// }
+//  console.log("End of program");
+// // let score=80;
+// if(score>=90){
+//     console.log("A grade");
+// }
+// console.log("End of program");
+
+// let score=80;
+// if(score>=90){
+//     console.log("A grade");
+// }
+// else{
+//     console.log("Not an A grade");
+// }
+// console.log("End of program");
+
+// let score=50;
+// if(score>=90){
+//     console.log("A grade");
+// }
+// else{
+//     console.log("Not an A grade");
+// }
+// console.log("End of program");  
+
+
+// let mark=40;
+// if (mark>=90){
+//     console.log("A grade");
+// }
+// else if(mark>=80){
+//     console.log("Not an A grade");
+// }
+// else if(mark>=70){
+//     console.log("Not an A grade");
+// }
+// console.log("mark E FAILLED");
+
+
+// switch statement
+// let grade = "B";
+// switch(grade){
+//     case "A":
+//         console.log("Excellent!");
+//         break;
+//     case "B":
+//         console.log("Good job!");
+//         break;
+//     case "C":
+//         console.log("You can do better!");
+//         break;
+//     default:
+//         console.log("Invalid grade");
+// }
+
+// let month = 5;
+// switch(month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");            
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+// }
+// console.log("End of program");
+
+// let score = 85;
+// switch(score>90){
+//     case true:
+//         console.log("A grade");
+//         break; 
+// }
+// console.log("End of program");
+
+// let score = 85;
+// switch(score>0){
+//     case true:
+//         console.log("Score is valid");
+//         break;
+//         case false:
+//         console.log("Score is invalid");
+//         break;
+// }
+// console.log("End of program");
+
+// let week="Monday";
+// switch(week){
+//     case "Monday":
+//         console.log("Today is Monday");
+
+// }
+// let week="Monday";
+// switch(week){
+//     case "monday":
+//         case "Monday":
+//         console.log("Today is Monday");
+// }
+
