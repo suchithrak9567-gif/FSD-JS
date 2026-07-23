5e26fc8d8d30ff0c350db1c70a429a3828881bba
suchithrak9567-gif <suchithrak9567@gmail.com>
2026-07-23 23:38:02 +0530

    add session-14 file

---
+++ session-14.js
@@ -0,0 +1,102 @@
+// let a=10;
+// let b=20;
+// // function
+// function greet(){
+//     // logic
+//     console.log("Hello World");
+// }
+// greet();
+//  function greet(){
+//     // logic
+//     console.log("Hello World");
+// }
+// incall
+// greet();
+// greet();
+// greet();
+
+// parametera- name
+// function greet(name){
+// console.log("Hello, "+name);
+// }
+// // function arrgument
+// greet("John");  
+// greet("arun");  
+
+
+// function greet(fname,iname){
+//     console.log("Hello, "+fname+" "+iname);
+// }
+
+// greet("John","Doe");
+// greet("arun","kumar");
+
+
+// function add(a,b){
+//     return a+b;
+// }
+// const result=add(10,20);
+// console.log(result);
+
+// function add(a,b){
+//     a+=10;  //a=a+10,10+10
+//     b+=20    //b=b+20,20+20
+//     return a+b; // 20+40=60
+// }
+// const result=add(10,20);
+// console.log(result);
+
+// not work reverse return
+
+
+// function add(a,b){
+//     return a+b; // 20+40=60
+//     a+=10;  //a=a+10,10+10
+//     b+=20    //b=b+20,20+20
+    
+// }
+// const result=add(10,20);
+// console.log(result);
+
+
+// function expression =verableki assign chyith use chayn
+// const greeings=function(name){
+//     console.log("Hello, "+name);
+// }
+// greeings("John");
+
+// arrow function
+
+// const greet = () => {
+//     console.log("Hello, " + name);
+// }
+// greet("John");
+// const greet = (name) => {
+//     console.log("Hello, " + name);
+// }
+// greet("John");
+
+
+// single statement arow shortcut
+// const add=(a,b)=> a+b;
+// const res=add(10,20);
+// console.log("Result: " + res);
+
+// const add=(a,b)=>{
+//     a+=10;
+//     b+=20;
+//     return a+b;
+// }
+// const res=add(10,20);
+// console.log("Result: " + res);
+
+// const gradecheck=(score)=>{
+//     if( score >=90) return "A grade";
+//     if(score>=80) return "B grade";
+//     if(score>=70) return "C grade";
+//     if(score>=60) return "D grade";
+//     return "F grade";
+// }
+// gradecheck(90);
+// const result=gradecheck(90);
+// console.log("Result: " + result);
